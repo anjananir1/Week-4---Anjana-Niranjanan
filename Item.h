396ae3d5a25fd336bca3d5e9e061ea5a377fbb79
anjananir1 <anjana.niranj@berkeley.edu>
2025-07-13 22:56:23 -0700

    Create Item.h

---
+++ Item.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+class Item {
+private:
+    string name;
+    int quantity;
+
+public:
+    Item();
+    Item(string itemName, int itemQty);
+    void display();
+};
+
+
+
